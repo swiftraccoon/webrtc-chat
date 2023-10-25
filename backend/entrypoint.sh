404d81fcb8ef0e1c4a245065e9c6c07f7a895b5b
swiftraccoon <swiftraccoon-gh@wnc.icu>
2023-10-25 00:53:04 -0400

    feat: Add debugging statements and error handling to entrypoint.sh
    - Added debug statements to check Python version, Django version, and environment variables.
    - Added error handling to check if manage.py exists before starting Django.
    - Modified runserver command to include verbosity level 3.
    - Included tail command to keep the script running.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+set -e
 
-# Echo debug information
 echo "Debug: Starting entrypoint.sh"
 
-# Start Django application
-python manage.py runserver 0.0.0.0:8000
+# Check Python version
+echo "Debug: Checking Python version..."
+python --version
+
+# Check Django version
+echo "Debug: Checking Django version..."
+python -m django --version
+
+# Check environment variables
+echo "Debug: Checking environment variables..."
+env
+
+# Check if manage.py exists
+if [ -f "manage.py" ]; then
+    echo "Debug: manage.py exists."
+else
+    echo "Debug: manage.py does not exist. Exiting."
+    exit 1
+fi
+
+# Start Django
+echo "Debug: About to start Django directly..."
+python manage.py runserver 0.0.0.0:8001 --verbosity 3 || (echo "Debug: Django failed to start" && exit 1)
+
+# Keep the script running
+tail -f /dev/null
